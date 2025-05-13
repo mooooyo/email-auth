468ad54b273beb734a75e8e2e7f7e76428f95ec6
mooyo <moooooooooyo@gmail.com>
2025-05-13 14:55:26 +0900

    update string for staging

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <h1>hahahhahahhaahah this is root page</h1>
+      <h1>hahahhahahhaahah this is staging page</h1>
       <p>hahahahaahhaahahahah</p>
     </>
   );
