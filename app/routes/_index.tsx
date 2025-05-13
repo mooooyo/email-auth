c2e960f27c273360168f696ce06c3e4879c8f156
mooyo <moooooooooyo@gmail.com>
2025-05-13 12:25:10 +0900

    change string for dev env

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <h1>hahahhahahhaahah this is root page</h1>
+      <h1>hahahhahahhaahah this is root page for dev env</h1>
       <p>hahahahaahhaahahahah</p>
     </>
   );
