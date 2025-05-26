873254051ca7dc283fe5caaa00e8ca92b7a197a7
mooooyo <moooooooooyo@gmail.com>
2025-05-26 17:29:37 +0900

    Dev (#8)
    * change string for dev env
    
    * dev update
    
    * husky test
    
    * add shadcn
    
    * fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,5 @@
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-import router from "./routes";
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import router from './routes';
 
-createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
